8c59c5a7334e46032be936c8f8b96605659ee8db
Mihnea <vanatorulcelbun@gmail.com>
2023-12-04 22:18:46 +0200

    refacut nume

---
+++ README.md
@@ -0,0 +1,9 @@
+# Lista De Exercitii
+
+
+1.
+Se citesc doua numere naturale m si n. Calculati n^m. (ex1.cpp)
+
+
+2.
+Se introduc de la tastatura n numere. Sa se afiseze valoarea minima si valoarea maxima. (ex2.cpp)
---
+++ ex2.cpp
@@ -0,0 +1,27 @@
+/*
+ * play2.cpp
+ *
+ *  Created on: Dec 4, 2023
+ *      Author: andi
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int	x;
+	int maxx;
+	int minn;
+	std::cout<<
+
+
+
+
+
+
+
+}
+
+
